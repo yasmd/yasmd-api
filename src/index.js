6e2acde30d8923ded8620f306150320ae9d4da5e
Samuel Olagunju <94651716+dioxair@users.noreply.github.com>
2023-06-04 17:52:55 -0400

    API now has basic functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
-const app = express();
+import ytdl from "ytdl-core";
 
-app.get("/", (req, res) => {
-  res.json({ error: "No input specified." });
+const app = express();
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  if (req.body.url !== undefined) {
+    res.setHeader("Content-disposition", "attachment; filename=video.mp4");
+    res.writeHead(200, { "Content-Type": "video/mp4" });
+    ytdl(req.body.url, {
+      quality: "highestvideo",
+      filter: "audioandvideo",
+    }).pipe(res);
+  } else {
+    res.json({ error: "No video URL defined" });
+  }
 });
 
 const PORT = process.env.PORT || 8080;
